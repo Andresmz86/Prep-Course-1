47e08ff23f30770cacf48acd164ceacf7dc7d02e
AndresMz <rp20v@hotmail.com>
2023-02-04 23:36:40 -0300

    second commit

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -2,7 +2,7 @@
 
 function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
-  //ej: Recibe "mario" ----> Devuelve "Mario"
+  //ej: Recibe "mario" ----> no vuelve "Mario"
   //Tu código:
 }
 
